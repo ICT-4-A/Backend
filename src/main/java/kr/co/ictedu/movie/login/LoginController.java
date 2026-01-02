24380c392abf1378c428afa875d7771d223ace00
esua <esua227@naver.com>
2026-01-02 14:22:03 +0900

    feat#30 : 로그인 로그아웃 구현

--- src/main/java/kr/co/ictedu/movie/login/LoginController.java
+++ src/main/java/kr/co/ictedu/movie/login/LoginController.java
@@ -3,11 +3,13 @@ package kr.co.ictedu.movie.login;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,11 +27,11 @@ public class LoginController {
 			@RequestHeader("User-Agent") String userAgent, @RequestBody MemberVO vo) {
 		Map<String, Object> result = loginservice.loginCheck(vo);
 		System.out.println("result =>" + result);
-
+		System.out.println("email: " + vo.getEmail());
+		System.out.println("password: " + vo.getPassword());
 		if (result != null && result.get("CNT") != null) {
 			int cnt = ((Number) result.get("CNT")).intValue();
 			if (cnt == 1) {
-				System.out.println("세션 처리 완료!");
 				vo.setNickname(result.get("NICKNAME").toString());
 				session.setAttribute("loginMember", vo);
 				return "success";
@@ -37,6 +39,8 @@ public class LoginController {
 		}
 		return "fail";
 	}
+
+
 
 	@GetMapping("/dologout")
 	public String doLogout(HttpSession session, HttpServletRequest request,
@@ -49,7 +53,9 @@ public class LoginController {
 	@GetMapping("/session")
 	public MemberVO session(HttpSession session) {
 		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
+		
 		if(loginMember != null) {
+			System.out.println("SessionGet :"+loginMember.getNickname());
 			System.out.println(loginMember.getNickname());
 			loginMember.setPassword(null);
 		}
