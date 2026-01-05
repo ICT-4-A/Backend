fe6fe321b7c1a2a3fc425b0461e97265d137fc1c
jsw0227 <zhalrtjddn01@gmail.com>
2026-01-05 13:36:43 +0900

    gitignore 파일 수정

--- src/main/java/kr/co/ictedu/movie/vo/MovieFormVO.java
+++ src/main/java/kr/co/ictedu/movie/vo/MovieFormVO.java
@@ -14,6 +14,6 @@ public class MovieFormVO {
 	private int writer, toge_writer; //기존 작업자 id, 공동 작업자 id
 	private String simple_review, review; // 한줄평, 일반 평가
 	private float rate;
-	private String created_at, updated_at, deleted_at;
 	private int hit;
+	private String created_at, updated_at, deleted_at;
 }
